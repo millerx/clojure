d67037917b1b4d192478e67c5e77f40851e99579
Chris Miller <chris.miller@inin.com>
2019-04-28 11:47:05 +0200

    Print board.

--- brave/pegthing/test/pegthing/io_test.clj
+++ brave/pegthing/test/pegthing/io_test.clj
@@ -4,6 +4,6 @@
 
 (deftest test-parse-move
   (testing "Parse move"
-    (is (= [[0 0] [2 2]] (parse-move "A0 C2")))
-    (is (= [[0 0] [2 2]] (parse-move "a0 c2"))) ; Lower case
+    (is (= [[0 0] [2 2]] (parse-move "A1 C3")))
+    (is (= [[0 0] [2 2]] (parse-move "a1 c3"))) ; Lower case
     (is (nil? (parse-move "54 c5"))))) ; Invalid move string
