cbcced9f32c07ce274cd0f2a802b43f57449a218
Chris Miller <chris.miller@inin.com>
2019-07-21 11:37:53 -0400

    Move basic-clj to it's own project.

--- basic-clj/src/evaluate.clj
+++
@@ -1,7 +0,0 @@
-(ns evaluate
-	"Functions to evaluate an AST with the interpeter state mache to produce a value.")
-
-(defn b-eval
-	"Evaluates an AST with the state machine and produces a value."
-	[ast]
-	ast)
